e30b9a488c60ceb5d0d5d1118b369866520f8347
Delila Halilovic <dhalilovic1@etf.unsa.ba>
2016-04-24 23:34:57 +0200

    Dodala tabelu za klasu Vozilo

---
+++ RentACar/App1/App1/RentACarBaza/Models/Vozilo.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1.RentACarBaza.Models
+{
+    class Vozilo
+    {
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        private int VoziloID { get; set; }
+        private double cijenaIznajmljivanja;
+        private string slika;
+        private Lokacija lokacija;
+
+
+        public double CijenaIznajmljivanja
+        {
+            get
+            {
+                return cijenaIznajmljivanja;
+            }
+
+            set
+            {
+                cijenaIznajmljivanja = value;
+            }
+        }
+
+        public string Slika
+        {
+            get
+            {
+                return slika;
+            }
+
+            set
+            {
+                slika = value;
+            }
+        }
+
+        internal Lokacija Lokacija
+        {
+            get
+            {
+                return lokacija;
+            }
+
+            set
+            {
+                lokacija = value;
+            }
+        }
+    }
+}
---
+++ RentACar/App1/App1/RentACarBaza/Models/VoziloDbContext.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace App1.RentACarBaza.Models
+{
+    class VoziloDbContext:DbContext
+    {
+        private DbSet<Vozilo> vozila;
+
+        internal DbSet<Vozilo> Vozila
+        {
+            get
+            {
+                return vozila;
+            }
+
+            set
+            {
+                vozila = value;
+            }
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string databaseFilePath = "Ooadbaza.db";
+
+            try
+            {
+                databaseFilePath = Path.Combine(ApplicationData.Current.LocalFolder.Path, databaseFilePath);
+            }
+
+            catch (InvalidOperationException) { }
+
+            //Sqlite baza            
+
+            optionsBuilder.UseSqlite($"Data source={databaseFilePath}");
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // modelBuilder.Entity<Restoran>().Property(p => p.Slika).HasColumnType("image");
+        }
+    }
+}
